ecfebb060886aff9dcaa2ed908bd3c33937101a4
RieriUnb <wesleyteixeira0020@gmail.com>
2024-05-07 18:03:35 -0300

    Correção, troquei rainhas.c por rainhas.h. segue a mesma ideia, verificar se o tabuleiro é uma solução valida para o problema das 8 rainhas

--- rainhas.c
+++ rainhas.c
@@ -1,8 +1,7 @@
-// rainhas.h
-#ifndef RAINHAS_H
-#define RAINHAS_H
+#include "rainhas.h"
 
-// Declaração da função que verifica o tabuleiro das rainhas
-int verificar_tabuleiro(char tabuleiro[8][8]);
-
-#endif // RAINHAS_H
+// Função que verifica se o tabuleiro é uma solução válida para o problema das 8 rainhas
+int verificar_tabuleiro(char tabuleiro[8][8]) {
+    // Implementação da função
+    return -1; // Retorno temporário para compilação
+}
