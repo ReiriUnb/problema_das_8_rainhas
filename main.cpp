fd64a1bf2bb200e6b7d3c7671bbc930a74b2f35d
RieriUnb <wesleyteixeira0020@gmail.com>
2024-05-07 20:56:16 -0300

    arrumando o conflito entre o arquivo testa_rainhas.cpp, com main.cpp, os dois estão chamando o catch_config main

--- main.cpp
+++ main.cpp
@@ -1,26 +1,27 @@
 #include "rainhas.h"
 #include <iostream>
 
-int main() {
-    char tabuleiro[8][8] = {
-        {'0', '1', '0', '0', '0', '0', '0', '0'},
-        {'0', '0', '0', '0', '1', '0', '0', '0'},
-        {'0', '0', '0', '0', '0', '0', '1', '0'},
-        {'0', '0', '0', '0', '0', '0', '0', '1'},
-        {'1', '0', '0', '0', '0', '0', '0', '0'},
-        {'0', '0', '0', '0', '0', '0', '0', '0'},
-        {'0', '0', '0', '0', '0', '0', '0', '0'},
-        {'0', '0', '0', '0', '0', '0', '0', '0'}
-    };
+// Remova a definição da função main()
+// int main() {
+//     char tabuleiro[8][8] = {
+//         {'0', '1', '0', '0', '0', '0', '0', '0'},
+//         {'0', '0', '0', '0', '1', '0', '0', '0'},
+//         {'0', '0', '0', '0', '0', '0', '1', '0'},
+//         {'0', '0', '0', '0', '0', '0', '0', '1'},
+//         {'1', '0', '0', '0', '0', '0', '0', '0'},
+//         {'0', '0', '0', '0', '0', '0', '0', '0'},
+//         {'0', '0', '0', '0', '0', '0', '0', '0'},
+//         {'0', '0', '0', '0', '0', '0', '0', '0'}
+//     };
 
-    int resultado = verificar_tabuleiro(tabuleiro);
-    if (resultado == 1) {
-        std::cout << "A configuração é uma solução válida para o problema das 8 rainhas.\n";
-    } else if (resultado == 0) {
-        std::cout << "A configuração não é uma solução válida para o problema das 8 rainhas.\n";
-    } else {
-        std::cout << "A entrada não é válida.\n";
-    }
+//     int resultado = verificar_tabuleiro(tabuleiro);
+//     if (resultado == 1) {
+//         std::cout << "A configuração é uma solução válida para o problema das 8 rainhas.\n";
+//     } else if (resultado == 0) {
+//         std::cout << "A configuração não é uma solução válida para o problema das 8 rainhas.\n";
+//     } else {
+//         std::cout << "A entrada não é válida.\n";
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
